13a7f8876b4bb81c68620c7508f496652a8e6cf8
Oskar Gewalli <oskar@gewalli.se>
2016-09-15 22:12:04 +0200

    Script.fsx

--- SemVer.FromAssembly/Script.fsx
+++ SemVer.FromAssembly/Script.fsx
@@ -1,13 +1,13 @@
 ﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
 // for more guidance on F# programming.
 
-open SemVer.FromAssembly
+//open SemVer.FromAssembly
 
-#r "../SampleProject/bin/Debug/SampleProject.dll"
+//#r "../SampleProject/bin/Debug/SampleProject.dll"
 
-#load "Magnitude.fs"
-#load "Compare.fs"
-#load "SurfaceArea.fs"
+//#load "Magnitude.fs"
+//#load "Compare.fs"
+//#load "SurfaceArea.fs"
 
 
-printfn "%A" (SurfaceArea.get (typeof<SampleProject.Class1>.Assembly))
+//printfn "%A" (SurfaceArea.get (typeof<SampleProject.Class1>.Assembly))
